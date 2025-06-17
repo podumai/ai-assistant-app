8005064c0f1b4a7ab4a14158b4f308e02e13399a
podumai <kirillsm05@gmail.com>
2025-06-17 14:34:28 +0000

    Changed VARCHAR(60) field to TEXT.

--- auth/db/auth_service_db.sql
+++ auth/db/auth_service_db.sql
@@ -5,9 +5,10 @@ CREATE SCHEMA IF NOT EXISTS auth;
 CREATE TABLE IF NOT EXISTS auth.users
 (
   userId UUID PRIMARY KEY,
-  login VARCHAR(60) NOT NULL,
-  password VARCHAR(60) NOT NULL,
-  CONSTRAINT uniqueLogin UNIQUE(login)
+  login TEXT NOT NULL,
+  password TEXT NOT NULL,
+  CONSTRAINT uniqueLogin UNIQUE(login),
+  CONSTRAINT uniquePassword UNIQUE(password),
   CONSTRAINT validLogin CHECK(LENGTH(login) > 0),
   CONSTRAINT validPassword CHECK(LENGTH(password) > 0)
 );
